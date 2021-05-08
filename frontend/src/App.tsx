930285e7a8f5cc3beacef4971030e60ce70b465f
k31tha <keith@kdasoft.co.uk>
2021-05-09 00:08:11 +0100

    added useEffect and logging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <ClubDetailsContainer clubUrl={'woking'} />
+      <ClubDetailsContainer clubUrl={'knaphill-fc'} />
     </div>
   );
 }
--- frontend/src/containers/ClubDetailsContainer.tsx
+++ frontend/src/containers/ClubDetailsContainer.tsx
@@ -25,25 +25,40 @@ export type Props = {
 const ClubDetailsContainer = ({clubUrl}: Props): JSX.Element => {
   // Hold Club Details in State
   // set type
-  const [clubDetails, setClubDetails] = React.useState<Array<ClubDetail>>([
-    {
-      clubId: 1,
-      clubName: 'woking',
-      clubAddress: 'address',
-      clubLogo: null,
-      clubUrl: 'woking',
-    },
-    {
-      clubId: 2,
-      clubName: 'knaphill fc',
-      clubAddress: 'address',
-      clubLogo: null,
-      clubUrl: 'knaphill-fc',
-    },
-  ]);
-
+  console.log('before set state ' + clubUrl);
+  const [clubDetails, setClubDetails] = React.useState<Array<ClubDetail>>();
+  console.log('after set state ' + clubUrl);
+  React.useEffect(() => {
+    console.log('in useeffect before set state club details ' + clubUrl);
+    setClubDetails([
+      {
+        clubId: 1,
+        clubName: 'woking',
+        clubAddress: 'address',
+        clubLogo: null,
+        clubUrl: 'woking',
+      },
+      {
+        clubId: 2,
+        clubName: 'knaphill fc',
+        clubAddress: 'address',
+        clubLogo: null,
+        clubUrl: 'knaphill-fc',
+      },
+    ]);
+    console.log('in useeffect after set state club details ' + clubUrl);
+  }, [clubUrl]);
+  if (clubDetails === undefined) {
+    console.log('clubDetails undefined ' + clubUrl);
+    return (
+      <>
+        <p>club details not setup</p>
+      </>
+    );
+  }
   const club = clubDetails.find(club => club.clubUrl === clubUrl);
   if (club === undefined) {
+    console.log('club undefined ' + clubUrl);
     return (
       <>
         <p>club not found {clubUrl}</p>
