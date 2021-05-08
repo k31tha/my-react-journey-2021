f7da4cd042077e025bc2975b143ddb536f616e89
k31tha <keith@kdasoft.co.uk>
2021-05-08 21:16:07 +0100

    useState to hold data

--- frontend/src/containers/ClubDetailsContainer.tsx
+++ frontend/src/containers/ClubDetailsContainer.tsx
@@ -5,25 +5,44 @@ export type Props = {
   clubUrl: string;
 };
 
-const clubDetail: Array<ClubDetail> = [
-  {
-    clubId: 1,
-    clubName: 'woking',
-    clubAddress: 'address',
-    clubLogo: null,
-    clubUrl: 'woking',
-  },
-  {
-    clubId: 2,
-    clubName: 'knaphill fc',
-    clubAddress: 'address',
-    clubLogo: null,
-    clubUrl: 'knaphill-fc',
-  },
-];
+// const clubDetail: Array<ClubDetail> = [
+//   {
+//     clubId: 1,
+//     clubName: 'woking',
+//     clubAddress: 'address',
+//     clubLogo: null,
+//     clubUrl: 'woking',
+//   },
+//   {
+//     clubId: 2,
+//     clubName: 'knaphill fc',
+//     clubAddress: 'address',
+//     clubLogo: null,
+//     clubUrl: 'knaphill-fc',
+//   },
+// ];
 
 const ClubDetailsContainer = ({clubUrl}: Props): JSX.Element => {
-  const club = clubDetail.find(club => club.clubUrl === clubUrl);
+  // Hold Club Details in State
+  // set type
+  const [clubDetails, setClubDetails] = React.useState<Array<ClubDetail>>([
+    {
+      clubId: 1,
+      clubName: 'woking',
+      clubAddress: 'address',
+      clubLogo: null,
+      clubUrl: 'woking',
+    },
+    {
+      clubId: 2,
+      clubName: 'knaphill fc',
+      clubAddress: 'address',
+      clubLogo: null,
+      clubUrl: 'knaphill-fc',
+    },
+  ]);
+
+  const club = clubDetails.find(club => club.clubUrl === clubUrl);
   if (club === undefined) {
     return (
       <>
