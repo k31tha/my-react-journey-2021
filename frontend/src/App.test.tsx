a5342159d8228bd4f4227328076849881c1d4f02
k31tha <keith@kdasoft.co.uk>
2021-05-10 23:56:13 +0100

    look to resolve testing issues when a delay

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/woking/i);
+  const linkElement = await screen.findByText(/woking/i);
   expect(linkElement).toBeInTheDocument();
 });
